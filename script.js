7a4570562bf24845eb85690b8229887cdf42cedb
Md.Nazmul Hasan <49835508+nazmulcse041@users.noreply.github.com>
2024-02-04 01:32:43 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//Calculator Program
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
